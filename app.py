53cc493948a7154eade4d5ccdf0010385a3319ae
wagner <wagncarv@outlook.com>
2024-01-29 18:02:48 -0300

    Login e autenticação de usuário

--- app.py
+++ app.py
@@ -1,12 +1,37 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from models.user import User
 from database import db
+from flask_login import LoginManager, login_user, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 
+login_manager = LoginManager()
 db.init_app(app)
+login_manager.init_app(app)
+
+# view login
+login_manager.login_view = 'login'
+
+# sessão -> conexão ativa
+@login_manager.user_loader
+def load_user(user_id):
+    print("uer id", user_id)
+    return User.query.get(int(user_id))
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            login_user(user)
+            return jsonify({"message": "Autenticação realizada com sucesso"})
+    return jsonify({"message": "Credenciais inválidas"}), 400
 
 # Iniclialização manual, ambiente dev
 if __name__ == "__main__":
