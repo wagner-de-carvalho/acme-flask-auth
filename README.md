19f8579228182c3eb4b12df54634eb8bfc21f4e8
wagner <wagncarv@outlook.com>
2024-01-29 13:11:10 -0300

    Configuração sqlite, criação tabela user

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # acme-flask-auth
 API de autenticação com banco de dados
+
+### Criar banco de dados via flask shell com Flask Alchemy
+1 - `flask shell` = abre o shell do flask
+2 - `db.create_all()` = cria o banco de dados
+3 - `db.session` = objeto de sessão do banco de dados
+4 - `db.session.commit()` = executa as operações no banco de dados
+
--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from models.user import User
+from database import db
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
-db = SQLAlchemy(app)
 
-@app.route('/hello', methods=['GET'])
-def hello():
-    return 'Hello'
+db.init_app(app)
 
 # Iniclialização manual, ambiente dev
 if __name__ == "__main__":
